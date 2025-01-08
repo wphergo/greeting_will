7c719b4d20f72662b18b00c105f0e66e99355af0
wphergo <william.hergott@cse-cst.gc.ca>
2025-01-08 16:11:29 -0500

    bootcamp 2

---
+++ src/test/java/com/wphergo/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wphergo.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
