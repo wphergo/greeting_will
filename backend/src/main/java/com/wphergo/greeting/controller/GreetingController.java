8fd786d07764a12fe49e48cf357eef8e9b982988
wphergo <william.hergott@cse-cst.gc.ca>
2025-01-09 15:54:21 -0500

    made blank frontend

--- backend/src/main/java/com/wphergo/greeting/controller/GreetingController.java
+++ backend/src/main/java/com/wphergo/greeting/controller/GreetingController.java
@@ -1,8 +1,5 @@
 package com.wphergo.greeting.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wphergo.greeting.bo.GreetingBO;
 import com.wphergo.greeting.model.Greeting;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping(value = "/greeting")
+@RequiredArgsConstructor
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
-	
-	@Autowired
-	private GreetingBO greetingBO;
+
+	private final GreetingBO greetingBO;
 
 	@GetMapping("/{id}")
 	public Greeting getGreetingById(@PathVariable Long id) {
